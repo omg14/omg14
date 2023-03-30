a03f0888376f884185b8af2f089c9ccad64c14d9
Om Gupta <89613253+omg14@users.noreply.github.com>
2023-03-30 18:44:21 +0530

    Create input-output.cpp
    To read/write a text file using cpp .

---
+++ input-output.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+
+  // For getting input from input.txt file
+  freopen("input.txt", "r", stdin);
+
+  // Printing the Output to output.txt file
+  freopen("output.txt", "w", stdout);
+
+#endif
+
+// output to print 
+
+// example 
+  for (int j = 0; j < 800; j++)
+  {
+    for (int i = 1; i <= 900; i++)
+    {
+      cout << 404550 << endl;
+    }
+  }
+}
